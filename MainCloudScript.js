1134650769587eefe34656d5908e09e9fe543423
joker2xteam <joker2xteam@gmail.com>
2016-11-04 09:49:19 +0700

    Update MainCloudScript.js

--- MainCloudScript.js
+++ MainCloudScript.js
@@ -34,15 +34,13 @@ var roomID = args.roomID;
   var listInvite1 = args.listInvite;
   var dataPayload = {};
   dataPayload["room"] = roomID;
-  //for(i=0;i<listInvite1.length;i++){
-    //var playfabID = listInvite[i];
-    //server.UpdateUserData({
-    //  PlayFabId : playfabID,
-    //  Data : dataPayload
-    //});
-  //}
-  log.debug("roomID:", { roomID: roomID });
-  log.debug("sendInvite2:", { input2: dataPayload });
+  for(i=0;i<listInvite1.length;i++){
+    var playfabID = listInvite[i];
+    server.UpdateUserData({
+     PlayFabId : playfabID,
+      Data : dataPayload
+    });
+  }
   log.debug("args",{args:args});
   return {Message:listInvite1.length};
 }
